4493ae71bd86c2e2e326b86b18f9d38ed57d10f8
Sungur Cihan <nsc1169@gmail.com>
2021-09-02 12:49:05 +0300

    Exception ve Log aspectleri eklendi

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -2,6 +2,7 @@ using Business.Abstract;
 using Business.Concrete;
 using Core.DependencyResolvers;
 using Core.Extensions;
+using Core.Extensions.ErrorExtensions;
 using Core.Utilities.IoC;
 using Core.Utilities.Security.Encyription;
 using Core.Utilities.Security.Jwt;
@@ -81,6 +82,8 @@ namespace WebAPI
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebAPI v1"));
             }
+
+            app.ConfigureCustomExceptionMiddleware();
 
             app.UseCors(builder => builder.WithOrigins("http://localhost:44332").AllowAnyHeader());
 
